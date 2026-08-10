0631267dfa7cc39370521bd07a78192d8d018f86
Ravichandran Thirumalai <98630348+ravichandrant1697@users.noreply.github.com>
2026-08-10 15:07:06 +0530

    Update option_dashboard.js
    Unwanted method removed

--- option_dashboard.js
+++ option_dashboard.js
@@ -143,7 +143,6 @@ if (mode === "backtest") {
     applyEnvConfig();
     activateHorizon(process.env.HORIZON);
 
-    await requireValidToken();
 
     console.log("Loading workbook...");
     loadWorkbookCache();
